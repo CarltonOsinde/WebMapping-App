e7790925110ca0da5a9b9101682192a711aec97e
Carlton Osinde <47374849+CarltonOsinde@users.noreply.github.com>
2022-10-04 19:42:53 -0400

    Volcanoes Mapped

--- Map1.py
+++ Map1.py
@@ -1,10 +1,25 @@
+import pandas as pd
 import folium
+
+raw_data1 = pd.read_csv("/Volcanoes.txt")
+Lat = list(raw_data1["lat"])
+Lon = list(raw_data1["lon"])
+elv = list(raw_data1["ELEV"])
+
+def color_producer(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif 1000<= elevation <=3000:
+        return 'yellow'
+    else:
+        return 'red'
+
 
 #Folium is a python library that allows you to create Maps from python code that are rendered into HTML, JavaScript and CSS
 #location's are stored in a list
 #-------------------------------------------------------------------------------------------------------------------------#
 
-map = folium.Map(location = [56.1304, -106.3468],zoom_start = 4 ,tiles = "Stamen Terrain")
+m = folium.Map(location = [56.1304, -106.3468],zoom_start = 4 ,tiles = "Stamen Terrain")
 
 #Adding Elements to the map object ~ An object allows us to apply method's and functions to do something or show something
 #-------------------------------------------------------------------------------------------------------------------------#
@@ -13,11 +28,23 @@ map = folium.Map(location = [56.1304, -106.3468],zoom_start = 4 ,tiles = "Stamen
 #-------------------------------------------------------------------------------------------------------------------------#
 
 fg = folium.FeatureGroup(name="My Map")
-fg.add_child(folium.Marker(location = [43.6532, -79.3832], popup="I'm a marker styl", icon=folium.Icon(color ='blue')))
 
-map.add_child(fg)
 
-map.save("Map1.html")    #.save() method creates and generates a world map, zooming in on the longitude and latitude stated above in the .Map() method
+for lt, ln, el in zip(Lat, Lon, elv):
+    fg.add_child(folium.Marker(location = [lt,ln], popup=str(el) + "m", icon=folium.Icon(color = color_producer(el))))
+
+m.add_child(fg)
+
+
+
+
+
+
+#Rendering Python code to HTML, CSS and JavaScript ~ something the browser can understand 
+#NOTE:  #.save() method creates and generates a world map, zooming in on the longitude and latitude stated above in the .Map() method
+#-------------------------------------------------------------------------------------------------------------------------#
+
+m.save("Map1.html")   
 
 
 
